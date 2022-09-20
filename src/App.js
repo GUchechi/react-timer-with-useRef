0350e70b81660cea8b404652c1084dffeed724f6
GUchechi <godswilluchendu7@gmail.com>
2022-09-20 10:20:02 +0100

    added useRef to get focus

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
   const [randomInput, setRandomInput] = useState("")
+  const render = useRef(0)
+  const inputRef = useRef()
 
   const handleChange = (e) => {
     const random = e.target.value;
     setRandomInput(random);
+    render.current++;
   }
+
+  const focusOnInput = () => {
+    inputRef.current.focus();
+  }
+
   return (
     <main className="App">
       <input 
+        ref={inputRef}
         type="text"
         value={randomInput}
         placeholder="Random input"
         onChange={handleChange}
       />
+      <p>Renders: {render.current}</p>
       <br /><br />
-
+      <button onClick={focusOnInput}>Focus</button>
       <p>{randomInput}</p>
 
     </main>
